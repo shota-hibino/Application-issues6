e08758e93c1f7720250aaf145f3c6e01f7e103ab
shota-hibino <yamoushou@gmail.com>
2021-05-07 06:23:38 +0000

    [Finish]comment function Asynchronous communication create

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,21 +1,23 @@
 class BookCommentsController < ApplicationController
+
   def create
-    @book = Book.find(params[:book_id])
-    comment = BookComment.new(book_comment_params)
-    comment.user_id = current_user.id
-    comment.book_id = @book.id
-    comment.save
-    redirect_to book_path(@book)
+  	@book = Book.find(params[:book_id])
+  	@book_comment = BookComment.new(book_comment_params)
+  	@book_comment.book_id = @book.id
+  	@book_comment.user_id = current_user.id
+  	@book_comment.save
+  	render :book_comment
   end
 
-  def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
-  end
+def destroy
+	@book = Book.find(params[:book_id])
+  	book_comment = @book.book_comments.find(params[:id])
+	book_comment.destroy
+	render :book_comment
+end
 
-    private
-
-  def book_comment_params
-    params.require(:book_comment).permit(:comment)
-  end
+	private
+	def book_comment_params
+		params.require(:book_comment).permit(:comment)
+	end
 end
